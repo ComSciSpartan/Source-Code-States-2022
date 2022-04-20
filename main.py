8562432d970e924c9f1e3cea3d38065ff7ab7e86
Randy Carnahan <carnah13@msu.edu>
2022-04-19 23:48:11 -0400

    More questions

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ If you have any questions please raise your hand. You are allowed to use the int
 however you see fit. Best of luck!
 """
 
+import math
+
+teamName = "ENTER TEAM NAME"
+teamNumber = "ENTER TEAM NUMBER"
+ourNames = "ENTER YOUR NAME/NAMES IF A PAIR"
+
 """
 Simple Function
 Make the function return True.
@@ -18,7 +24,7 @@ def question1():
 
 """
 Fibonacci Numbers
-Calculate the first 20 Fibonnaci numbers and put them in a list. Simply adding the first 20 to a
+Calculate the first 20 Fibonacci numbers and put them in a list. Simply adding the first 20 to a
 list will result in no points. You must use a for loop in order to receive credit. 
 """
 def question2():
@@ -28,7 +34,47 @@ def question2():
 """
 Converting Types
 A list of numbers "data" will be passed in. Convert the list of strings to a list of integers and 
-return it. You
+return it. You must perform a conversion on each list item to receive points. 
 """
 def question3(data):
     pass
+
+
+"""
+Calculations
+Two points will be passed into this function. Use the distance formula to calculate the distance
+between the two points and return it. The math library is already imported for you. 
+"""
+def question4(x1, y1, x2, y2):
+    pass
+
+
+"""
+Prime Numbers
+A random number "data" will be passed in. Create an algorithm to check if the number is prime.
+Return True if it is prime and False otherwise. Hard coding the inputs to return a certain value
+will result in no points. Ex: if(input == 2) return True, is not allowed.
+"""
+def question4(data):
+    pass
+
+
+"""
+Build a Dictionary
+Create a dictionary from the passed in list "data" and return it. The dictionary key is the value's
+index in the list and the dictionary value is the list element at that index. Ex the list
+[red, green, blue] is passed in. The resulting dictionary would be {'0' : 'red', '1' : 'green', 
+'2' : 'blue'}
+"""
+def question5(data):
+    pass
+
+
+def main():
+    print(teamName)
+    print(teamNumber)
+    print(ourNames)
+
+
+if 1 == 1:
+    main()
