ad26fddb2870d1df49bbe172f951a04a0674ba16
Randy Carnahan <carnah13@msu.edu>
2022-04-19 23:18:07 -0400

    Base functions just pushing as I go

---
+++ main.py
@@ -0,0 +1,34 @@
+"""
+Welcome to the 2022 Michigan State Source Code competition. Please make sure that you
+fill out the variables before so that your grade can be recorded accurately.
+
+Please remove the pass statement from any function that you attempt to complete.
+
+If you have any questions please raise your hand. You are allowed to use the internet
+however you see fit. Best of luck!
+"""
+
+"""
+Simple Function
+Make the function return True.
+"""
+def question1():
+    pass
+
+
+"""
+Fibonacci Numbers
+Calculate the first 20 Fibonnaci numbers and put them in a list. Simply adding the first 20 to a
+list will result in no points. You must use a for loop in order to receive credit. 
+"""
+def question2():
+    pass
+
+
+"""
+Converting Types
+A list of numbers "data" will be passed in. Convert the list of strings to a list of integers and 
+return it. You
+"""
+def question3(data):
+    pass
